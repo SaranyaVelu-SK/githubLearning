05850b4f0e52c0d2d55d1d949b79177a708ee939
SaranyaVelu-SK <saranyakulanthaivelu@gmail.com>
2024-05-25 10:37:40 +0530

    learnt to use react components

--- App.js
+++ App.js
@@ -1,24 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 
+// const parentTag = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child1" }, [
+//     React.createElement(
+//       "h1",
+//       { id: "heading1" },
+//       "Learning React is super cool"
+//     ),
+//     React.createElement("h2", { id: "heading2" }, "Excited to learn more"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement(
+//       "h1",
+//       { id: "heading1" },
+//       "Learning React is super cool"
+//     ),
+//     React.createElement("h2", { id: "heading2" }, "Excited to learn more"),
+//   ]),
+// ]);
 
-const parentTag = React.createElement('div',{id:'parent'},
-    [
-        React.createElement('div',{id:'child1'},
-            [
-                React.createElement('h1',{id : 'heading1'},'Learning React is super cool'),
-                React.createElement('h2',{id : 'heading2'},'Excited to learn more')
-            ]
-        ),
-        React.createElement('div',{id:'child2'},
-            [
-                React.createElement('h1',{id : 'heading1'},'Learning React is super cool'),
-                React.createElement('h2',{id : 'heading2'},'Excited to learn more')
-            ]
-        )
-    ]
-)
+const FirstFuncComp = () => (<h1>Welcome saro!!!</h1>);
+function WrapperComp () {
+  return (
+    <div>
 
-        console.log(parentTag)
-        const reactRoot = ReactDOM.createRoot(document.getElementById('mainroot'));
-        reactRoot.render(parentTag)
+      {FirstFuncComp()}
+      <h2 style={{color:'red',backgroundColor:'aquamarine'}}>Happy to host you</h2>
+    </div>
+  )
+}
 
-    
+const reactRoot = ReactDOM.createRoot(document.getElementById("mainroot"));
+reactRoot.render(<WrapperComp ></WrapperComp>);
